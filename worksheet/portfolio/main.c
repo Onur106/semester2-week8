a5062ff7936befdb01ab4303690af0caa2d03675
Onur106 <cmft0160@leeds.ac.uk>
2026-03-26 20:44:06 +0000

    end of week8

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -1,14 +1,16 @@
-
 #include <stdio.h>
 #include <stdbool.h>
-
 #include "shapes.h"
 
-int main( void ) {
+int main() {
+    Point p1 = makePoint(0, 0);
+    Point p2 = makePoint(3, 4);
 
-    Point p1 = makePoint(0.0,0.0);
-    
-    // test your code by calling the functions and printing the output 
-    
+    Line l = makeLine(p1, p2);
+    printf("Length: %.2f\n", lineLength(l));
+
+    Triangle t = makeTriangle(p1, p2, makePoint(0, 4));
+    printf("Area: %.2f\n", triangleArea(t));
+
     return 0;
 }
